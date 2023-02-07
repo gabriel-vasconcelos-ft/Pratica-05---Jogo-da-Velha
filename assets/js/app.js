ea0853e829ea87874a9f79ebddda4b11ffbbb221
Gabriel Vasconcelos <97545157+gabriel-vasconcelos-ft@users.noreply.github.com>
2023-02-07 19:34:37 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,27 +4,30 @@ let pontos2 = document.querySelector("#pontos_player_2");
 let tab = document.querySelector("#tabuleiro");
 let casa = document.querySelectorAll(".casa");
 
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 let jvArray = new Array(9);
 let vez = 1;
 let pnt1 = 0;
 let pnt2 = 0;
 
 function fillArray(position) {
-  if (vez === 1) {
-    jvArray[position] = "X";
-    vez = 2;
-  } else {
-    jvArray[position] = "O";
-    vez = 1;
-  }
+  jvArray[position] = vez === 1 ? "X" : "O";
+  vez = vez === 1 ? 2 : 1;
 }
 
 function checkGameOver() {
   if (checkWin()) {
-    if (vez !== 1) {
+    if (vez === 2) {
       pnt1++;
-      const span = document.createElement("span");
-      span.innerHTML = pnt1;
       pontos1.innerHTML = `
         Jogador 1
         <span>${pnt1}</span>
@@ -33,8 +36,6 @@ function checkGameOver() {
       alert("Vitória do Player 1");
     } else {
       pnt2++;
-      const span = document.createElement("span");
-      span.innerHTML = pnt2;
       pontos2.innerHTML = `
         Jogador 2
         <span>${pnt2}</span>
@@ -50,100 +51,41 @@ function checkGameOver() {
 }
 
 function checkWin() {
-  if (
-    jvArray[0] === jvArray[1] &&
-    jvArray[1] === jvArray[2] &&
-    jvArray[0] !== undefined
-  ) {
-    return true;
-  } else if (
-    jvArray[3] === jvArray[4] &&
-    jvArray[4] === jvArray[5] &&
-    jvArray[3] !== undefined
-  ) {
-    return true;
-  } else if (
-    jvArray[6] === jvArray[7] &&
-    jvArray[7] === jvArray[8] &&
-    jvArray[6] !== undefined
-  ) {
-    return true;
-  } else if (
-    jvArray[0] === jvArray[3] &&
-    jvArray[3] === jvArray[6] &&
-    jvArray[0] !== undefined
-  ) {
-    return true;
-  } else if (
-    jvArray[1] === jvArray[4] &&
-    jvArray[4] === jvArray[7] &&
-    jvArray[1] !== undefined
-  ) {
-    return true;
-  } else if (
-    jvArray[2] === jvArray[5] &&
-    jvArray[5] === jvArray[8] &&
-    jvArray[2] !== undefined
-  ) {
-    return true;
-  } else if (
-    jvArray[0] === jvArray[4] &&
-    jvArray[4] === jvArray[8] &&
-    jvArray[0] !== undefined
-  ) {
-    return true;
-  } else if (
-    jvArray[2] === jvArray[4] &&
-    jvArray[4] === jvArray[6] &&
-    jvArray[2] !== undefined
-  ) {
-    return true;
-  } else {
-    return false;
+  for (let i = 0; i < winConditions.length; i++) {
+    const [a, b, c] = winConditions[i];
+    if (
+      jvArray[a] === jvArray[b] &&
+      jvArray[b] === jvArray[c] &&
+      jvArray[a] !== undefined
+    ) {
+      return true;
+    }
   }
+  return false;
 }
 
 function checkDraw() {
-  let draw = true;
-  for (let i = 0; i < jvArray.length; i++) {
-    if (jvArray[i] == undefined) {
-      draw = false;
-    }
-  }
-  return draw;
+  return jvArray.every(casa => casa !== undefined);
 }
 
 function clearGame() {
   jvArray = new Array(9);
   vez = 1;
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].classList.remove("casa_selecionada");
-    cells[i].classList.remove("x");
-    cells[i].classList.remove("o");
-  }
+  casa.forEach(c => {
+    c.classList.remove("casa_selecionada");
+    c.classList.remove("x");
+    c.classList.remove("o");
+  });
 }
 
 function startGame() {
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].classList.remove("x");
-    cells[i].classList.remove("o");
-    cells[i].addEventListener("click", function () {
-      console.log(vez);
+  casa.forEach((c, i) => {
+    c.addEventListener("click", function() {
+      fillArray(i);
       this.classList.add("casa_selecionada");
-      if (typeof jvArray[i] == "undefined") {
-        if (vez === 1) {
-          this.classList.add("x");
-        } else {
-          this.classList.add("o");
-        }
-        fillArray(i);
-        checkGameOver();
-      }
+      this.classList.add(vez === 1 ? "x" : "o");
+      checkGameOver();
     });
-  }
+  });
 }
-
-startGame();
-btn.addEventListener("click", function () {
-  clearGame();
-});
+``
